8e1f87d813e8f6992f57de5df0a4f73d608f6a17
Dan Ahlquist <dan.ahlquist@gmail.com>
2020-11-19 22:27:22 -0600

    Setup for Week 4 - Graph Min Cut

---
+++ week/4/Week 4 - Graph Min Cut/src/AdjacencyListGraph.kt
@@ -0,0 +1,66 @@
+import java.lang.IllegalStateException
+
+class AdjacencyListGraph<T>: MutableGraph<T> {
+
+    private val nodes: MutableMap<T, Node<T>> = mutableMapOf()
+
+    override fun hasNode(a: T): Boolean {
+        return nodes.contains(a)
+    }
+
+    override fun hasEdge (a: T, b: T): Boolean {
+        return nodes.contains(a) && nodes[a]?.hasNeighbor(b) ?: false
+    }
+
+    override fun getNeighborsOf(a: T): List<T> {
+        return nodes[a]?.getNeighbors() ?: emptyList()
+    }
+
+    override fun addNode(a: T) {
+        if (nodes.contains(a))
+            throw IllegalStateException("This Graph already contains a node $a")
+        else
+            nodes[a] = Node(a)
+    }
+
+    override fun addEdge (a: T, b: T) {
+        addNodeEnd(a, b)
+        addNodeEnd(b, a)
+    }
+
+    private fun addNodeEnd(x: T, y: T) {
+        if(nodes.contains(x)) {
+            nodes[x]?.addNeighbor(y)
+        } else {
+            val new = Node(x)
+            new.addNeighbor(y)
+            nodes[x] = new
+        }
+    }
+
+    override fun deleteNode(a: T): Boolean {
+        val result = nodes.contains(a)
+
+        nodes.remove(a)
+        for (node in nodes.values)
+            node.deleteNeighbor(a)
+
+        return result
+    }
+
+    override fun deleteEdge (a: T, b: T): Boolean {
+        val result = this.hasEdge(a, b) || this.hasEdge(b, a)
+        nodes[a]?.deleteNeighbor(b)
+        nodes[b]?.deleteNeighbor(a)
+        return result
+    }
+
+    private class Node<T> (val name: T) {
+        private val neighbors: MutableSet<T> = mutableSetOf()
+
+        fun hasNeighbor(n: T): Boolean { return neighbors.contains(n) }
+        fun getNeighbors(): List<T> { return neighbors.toList() }
+        fun addNeighbor(n: T) { neighbors.add(n) }
+        fun deleteNeighbor(n: T): Boolean { return neighbors.remove(n) }
+    }
+}
---
+++ week/4/Week 4 - Graph Min Cut/src/MutableGraph.kt
@@ -0,0 +1,15 @@
+interface MutableGraph<T> {
+    fun hasNode(a: T): Boolean
+
+    fun hasEdge(a: T, b: T): Boolean
+
+    fun getNeighborsOf(a: T): List<T>
+
+    fun addNode(a: T)
+
+    fun addEdge(a: T, b: T)
+
+    fun deleteNode(a: T): Boolean
+
+    fun deleteEdge(a: T, b: T): Boolean
+}
