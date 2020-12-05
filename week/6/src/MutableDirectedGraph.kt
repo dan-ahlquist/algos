2a3592b4dc7171cc950bebc1340a1608040bb6d0
Dan Ahlquist <dan.ahlquist@gmail.com>
2020-12-05 14:07:14 -0600

    Week 6 - start

---
+++ week/6/src/MutableDirectedGraph.kt
@@ -0,0 +1,24 @@
+import org.graalvm.compiler.graph.Edges
+
+data class Edge<T, W> (val from: Node<T>, val to: Node<T>, val weight: W)
+
+class Node<T> (
+    val data: T,
+    var isExplored: Boolean = false
+)
+
+interface MutableDirectedGraph<T, W> {
+    val nodeCount: Int
+    fun getNodes(): Set<Node<T>>
+    fun hasNode(a: T): Boolean
+    fun hasEdge(a: T, b: T): Boolean
+    fun getEdgesFrom(a: Node<T>): Set<Edge<T, W>>
+    fun addNode(a: T)
+
+    fun addEdge(a: T, b: T, weight: W)
+
+    fun resetExplored()
+    fun reverse()
+
+    fun print()
+}
---
+++ week/6/src/MutableDirectedGraphImpl.kt
@@ -0,0 +1,79 @@
+import java.lang.IllegalStateException
+
+class MutableDirectedGraphImpl<T, W>(
+        private val nodes: MutableMap<T, Node<T>> = mutableMapOf(),
+        private val edges: MutableList<Edge<T, W>> = mutableListOf()
+): MutableDirectedGraph<T, W> {
+
+    val edgesFrom = mutableMapOf<Node<T>, MutableSet<Edge<T, W>>>()
+
+    override fun getNodes(): Set<Node<T>> {
+        return nodes.values.toSet()
+    }
+
+    override val nodeCount: Int
+        get() = nodes.size
+
+    override fun hasNode(a: T): Boolean {
+        return nodes.containsKey(a)
+    }
+
+    override fun hasEdge(a: T, b: T): Boolean {
+        return edges.any { it.from.data == a && it.to.data == b }
+    }
+
+    override fun getEdgesFrom(a: Node<T>): Set<Edge<T, W>> {
+        return edgesFrom[a] ?: emptySet()
+    }
+
+    override fun addNode(a: T) {
+        if (hasNode(a)) throw IllegalStateException("This graph already has a node $a")
+        nodes[a] = Node(a)
+    }
+
+    override fun addEdge(a: T, b: T, weight: W) {
+        if (!nodes.contains(a)) { nodes[a] = Node(a) }
+        if (!nodes.contains(b)) { nodes[b] = Node(b) }
+
+        val aNode = nodes[a]
+        val bNode = nodes[b]
+        val newEdge = Edge(aNode!!, bNode!!, weight)
+        edges.add(newEdge)
+
+        val aEdges = edgesFrom.getOrPut(aNode, { mutableSetOf() })
+        aEdges.add(newEdge)
+    }
+
+    override fun resetExplored() {
+        nodes.values.forEach { n ->
+            n.isExplored = false
+        }
+    }
+
+    private fun resetNeighbors() {
+        nodes.values.forEach { n ->
+            edgesFrom.clear()
+        }
+    }
+
+    override fun reverse() {
+        resetNeighbors()
+
+        val oldEdges = edges.toMutableList() //shallow copy
+        edges.clear()
+        oldEdges.forEach {
+            addEdge(it.to.data, it.from.data, it.weight)
+        }
+    }
+
+    override fun print() {
+        println("${nodes.size} nodes, ${edges.size} edges.")
+        for (node in nodes.values) {
+            val sb = StringBuffer("${node.data} -> ")
+            for (e in edgesFrom[node] ?: emptySet()) {
+                sb.append("${e.to.data} ")
+            }
+            println(sb.toString())
+        }
+    }
+}
