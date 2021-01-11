2828769ef588705bd2524bb109b2b7d7b2a67839
Dan Ahlquist <dan.ahlquist@gmail.com>
2021-01-11 12:50:19 -0600

    WIP week 11 basics

---
+++ week/11/Week 11 - Huffman/src/HuffmanTree.kt
@@ -0,0 +1,11 @@
+
+//TODO eliminate nullable value
+class HuffmanTree(val symbol: Symbol?) {
+
+    var left: HuffmanTree? = null
+    var right: HuffmanTree? = null
+    
+    fun mergeAsSibling(other: HuffmanTree): HuffmanTree {
+        TODO()
+    }
+}
